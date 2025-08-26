eaf762c3996e575e0ce5ead14aca381d273a99db
Danya <danahmaruk91@gmail.com>
2025-08-26 14:47:15 +0300

    js change

--- js/task-1.js
+++ js/task-1.js
@@ -1,28 +1,31 @@
 const customer = {
-    username: "Mango",
-    balance: 24000,
-    discount: 0.1,
-    orders: ["Burger", "Pizza", "Salad"],
-    getBalance() {
+  username: "Mango",
+  balance: 24000,
+  discount: 0.1,
+  orders: ["Burger", "Pizza", "Salad"],
+
+  getBalance() {
     return this.balance;
-    },
-    getDiscount() {
+  },
+  getDiscount() {
     return this.discount;
-    },
-    setDiscount(value) {
-    discount = value;
-    },
-    getOrders() {
+  },
+  setDiscount(value) {
+    this.discount = value;
+  },
+  getOrders() {
     return this.orders;
-    },
-    addOrder(cost, order) {
-      this.balance -= cost - cost * discount;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost - cost * this.discount;
     this.orders.push(order);
-    },
+  },
 };
 
+// Перевірка
 customer.setDiscount(0.15);
-  console.log(customer.getDiscount()); // 0.15
+console.log(customer.getDiscount()); // 0.15
+
 customer.addOrder(5000, "Steak");
-  console.log(customer.getBalance()); // 19750
-  console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders());  // ["Burger", "Pizza", "Salad", "Steak"]
